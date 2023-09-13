63ed1f5a37bc328976584fef7f8b1b5b43a3faac
‘JonathanAlbertCross’ <j.a.cross@icloud.com>
2023-09-13 15:25:42 +0100

    JS update6

--- script.js
+++ script.js
@@ -18,3 +18,12 @@ if (time <= 11) {
 
 message = "Hello " + username + ". " + message;
 document.write(message);
+
+// greet the user
+const greet = document.getElementById("greet");
+
+greet.addEventListener("click", function () {
+    const joy = prompt("What sparks joy the most for you?")
+
+    alert("I agree, " + joy + " does spark joy!")
+})
