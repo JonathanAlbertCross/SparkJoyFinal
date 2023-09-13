e1ebb97337942d7fd66187fe469588736401b94b
‘JonathanAlbertCross’ <j.a.cross@icloud.com>
2023-09-13 14:56:49 +0100

    JS update

--- script.js
+++ script.js
@@ -6,4 +6,26 @@ greet.addEventListener("click", function () {
 
     alert("I agree, " + joy + " does spark joy!")
 })
+// 
+let username = prompt("What is your name?")
 
+let message;
+
+let username = prompt("What is your name?");
+// concatenation
+// console.log("Hello " + username);
+
+let time = prompt("What hour is it? (0-23)");
+
+if (time <= 11) {
+    message = "Good morning";
+} else if (time <= 17) {
+    message = "Good afternoon";
+} else if (time < 24) {
+    message = "Good evening";
+} else {
+    message = "My friend, that is not a time!";
+}
+
+message = "Hello " + username + ". " + message;
+document.write(message);
