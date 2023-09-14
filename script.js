19a0eb7658b5276d3a1fac10352106b9afe45fad
‘JonathanAlbertCross’ <j.a.cross@icloud.com>
2023-09-14 13:30:05 +0100

    JS update11

--- script.js
+++ script.js
@@ -1,29 +1,36 @@
-
-
-let username = prompt("What is your name?");
 // concatenation
 // console.log("Hello " + username);
 
-let time = prompt("You do want to change your life?");
+function greet() {
+    let username = prompt("Please enter your name.");
+    let message;
+    let messageTwo
+    let time = prompt("Do want to make positive changes in your life?");
+    if (time == "yes") {
+        messageOne = "Let's get going!";
+        messageTwo = "Fillout the form!";
+    } else if (time == "no") {
+        messageOne = "You like living in nonsense? ";
+        messageTwo = "That is disappointing!";
+    } else {
+        messageOne = "";
+        messageTwo = "Try again!";
+    }
+    message = "Dearest " + username + ",";
 
-let message;
-
-if (time == yes) {
-    message = "Feel free to fill out the form";
-} else if (time == no) {
-    message = "You like living in nonsense?";
-} else {
-    message = "My friend, that is not a time!";
+    document.write(message);
+    document.write("<br>");
+    document.write("<br>");
+    document.write(messageOne);
+    document.write("<br>");
+    document.write(messageTwo);
 }
 
-message = "Hello " + username + " -  " + message;
-document.write(message);
-
 // greet the user
-const greet = document.getElementById("greet");
+const greeter = document.getElementById("spark");
 
-greet.addEventListener("click", function () {
+function spark() {
     const joy = prompt("What sparks joy the most for you?")
 
     alert("I agree, " + joy + " does spark joy!")
-})
+}
