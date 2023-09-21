7ac62f3e5affeb85c6cb2a879c65755162d99a2b
Najma Saeed <najmasy20@gmail.com>
2023-09-21 15:04:26 +0100

    made changes

--- script.js
+++ script.js
@@ -2,43 +2,63 @@
 // console.log("Hello " + username);
 
 function greet() {
-    let username = prompt("Please enter your name.");
-    let message;
-    let messageTwo
-    let time = prompt("Do want to make positive changes in your life? 'yes' or 'no'");
-    if (time == "yes") {
-        messageOne = "Let's get going!";
-        messageTwo = "Fillout the form!";
-    } else if (time == "no") {
-        messageOne = "You like living in nonsense? ";
-        messageTwo = "Fill out the form to make sure! 😊";
-    } else {
-        messageOne = "";
-        messageTwo = "Try again!";
-    }
-    message = "Dearest " + username + ",";
+  let username = prompt("Please enter your name.");
+  alert("Hello " + username);
+  /*
+  let message;
+  let messageTwo;
+*/
+  let time = prompt(
+    "Do want to make positive changes in your life? 'yes' or 'no')"
+  ).toLowerCase();
 
-    document.write(message);
-    document.write("<br>");
-    document.write("<br>");
-    document.write(messageOne);
-    document.write("<br>");
-    document.write(messageTwo);
+  while (time !== "yes" && time !== "no") {
+    time = prompt("has to be yes or no").toLowerCase();
+  }
+
+  if (time == "yes") {
+    alert("Let's get going! Fillout the form!");
+  } else if (time == "no") {
+    alert("You like living in nonsense? Fill out the form to make sure!");
+  } else {
+    alert("Try again!");
+  }
+
+  console.log(time);
 }
+/*
+  message = "Dearest " + username + ",";
 
+  document.write(message);
+  document.write("<br>");
+  document.write("<br>");
+  document.write(messageOne);
+  document.write("<br>");
+  document.write(messageTwo);
+}
+*/
 // greet the user
 const greeter = document.getElementById("spark");
 
 function spark() {
-    const joy = prompt("What sparks joy the most for you?")
+  const joy = prompt("What sparks joy the most for you?");
 
-    alert("I agree, " + joy + " does spark joy!")
+  alert("I agree, " + joy + " does spark joy!");
 }
 
 function getRating() {
-    let rating = prompt('Rate my site between 1 - 5', '5');
+  let rating = prompt("Rate my site between 1 - 5", "5");
 
-    for (let i = 0; i < rating; i++) {
-        document.write("<img src = 'star.png'/>")
-    }
+  /*
+  while (!(rating > 0; i < rating; i++) {
+    document.write("<img src ='star.png' class='loops-image'/>");
+  }
+}
+*/
+  while (!(rating > 0 && rating <= 5)) {
+    rating = prompt("Please enter a number between 1 and 5!");
+  }
+  for (let counter = 0; counter < rating; counter++) {
+    document.write("<img src='star.png' class='loops-image'/>");
+  }
 }
